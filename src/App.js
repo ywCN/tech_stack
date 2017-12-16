91e4689a2bfd19a33db995cd939d5df58ef5238e
ywCN <wangyouhao90@gmail.com>
2017-12-15 19:49:02 -0500

    set up redux for react

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { View } from 'react-native';
+import { Provider } from 'react-redux'; // glue between react and redux
+import { createStore } from 'redux';
+import reducers from './reducers';
 
 const App = () => {
     return (
-        <View />
+        <Provider store={createStore(reducers)}>
+            <View />
+        </Provider>
     );
 };
 
