281ca768f4ea58b9e4abe3b735e23ffe247e1a80
ywCN <wangyouhao90@gmail.com>
2017-12-16 10:14:28 -0500

    rewrite reducer

--- src/reducers/SelectionReducer.js
+++ src/reducers/SelectionReducer.js
@@ -1,5 +1,11 @@
 export default (state, action) => {
-    console.log(action);
-    // cannot return undefined
-    return null; // null for we do not want a selected row when boot up
+    // console.log(action);
+    // // cannot return undefined
+    // return null; // null for we do not want a selected row when boot up
+    switch (action.type) {
+        case 'select_library':
+            return action.payload;
+        default:
+            return state;
+    }
 };
